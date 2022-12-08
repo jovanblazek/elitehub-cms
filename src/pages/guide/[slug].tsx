5cf3b6b6b8a815f22bfbc4b86aa4a4235f84a8d6
Jovan Blažek <36740941+jovanblazek@users.noreply.github.com>
2022-12-08 15:21:33 +0100

    Remove sanity

--- src/pages/guide/[slug].tsx
+++ src/pages/guide/[slug].tsx
@@ -1,30 +1,6 @@
-import { GetStaticPaths, GetStaticProps } from 'next'
-import sanityClient from 'utils/sanityClient'
-// eslint-disable-next-line no-restricted-exports
+// import { GetStaticPaths, GetStaticProps } from 'next'
 export { Guide as default } from 'routes/Guide'
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const paths: string[] = await sanityClient.fetch(
-    `*[_type == "guide" && defined(slug.current)][].slug.current`
-  )
+// export const getStaticPaths: GetStaticPaths = async () => {}
 
-  return {
-    paths: paths.map((slug) => ({ params: { slug } })),
-    fallback: true,
-  }
-}
-
-export const getStaticProps: GetStaticProps = async (context) => {
-  const { slug = '' } = context?.params || {}
-  const guide = await sanityClient.fetch(
-    `
-    *[_type == "guide" && slug.current == $slug][0]{title, body}
-  `,
-    { slug }
-  )
-  return {
-    props: {
-      guide,
-    },
-  }
-}
+// export const getStaticProps: GetStaticProps = async (context) => {}
--- src/utils/sanityClient.ts
+++
@@ -1,9 +0,0 @@
-import sanityClient from '@sanity/client'
-
-export default sanityClient({
-  projectId: 'nhbxzai9', // you can find this in sanity.json
-  dataset: 'production', // or the name you chose in step 1
-  useCdn: true, // `false` if you want to ensure fresh data
-  apiVersion: '2022-06-05',
-  token: process.env.SANITY_TOKEN,
-})
